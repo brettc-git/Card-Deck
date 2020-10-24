db50f1f26fcb755107a504247d8f7851bf10ee2f
brettc-git <72076153+brettc-git@users.noreply.github.com>
2020-10-24 01:29:41 -0700

    Update card.h

--- card.h
+++ card.h
@@ -1,21 +1,22 @@
 #pragma once
 #include <iostream>
-#include "card_deck.h"
+#include <string>
 using std::cout;
 using std::string;
 
 const string suits[] = { "S", "H", "C", "D" }; // Spades, Hearts, Clubs, Diamonds
-enum Face { Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King };
 
 
-class Card : public card_deck {
+class Card {
 
 private:
 	string suit; 
-	Face face;
+	int face;
 	
 public:
 	Card() : suit(), face() {};
-	Card(string s, Face num) : suit(s), face(num) {};
-	void printCard();
+	Card(string s, int num) : suit(s), face(num) {};
+	string printCard();
+	string getSuit();
+	int getFace();
 };
